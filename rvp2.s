00a6d09375f42f665db782bbec1e65c8bc20a09b
Eric Smith <ersmith@totalspectrum.ca>
2019-07-07 20:01:24 -0300

    fix ldscript template to always link interpreter first, and add _sbrk to interpreter

--- rvp2.s
+++ rvp2.s
@@ -8,3 +8,21 @@ _emustart:
 	.incbin "p2trace.bin"
 _emuend:	
 	
+
+	.text
+	.align	2
+	.globl _sbrk
+	.globl _end
+_sbrk:
+	la	a1, _heap_ptr
+	lw	a2, 0(a1)
+	bne	x0,a2,skipset
+	la	a2, _end
+skipset:
+	add	a2, a0, a2
+	sw	a2, 0(a1)
+	ret
+
+	.data
+_heap_ptr:	
+	.long	0
